74a1a26e04d931d19724615376c118e9414108de
Max Horowitz <maxhorowitz@wireless-67-64.resnet.wisc.edu>
2015-01-25 23:02:17 -0600

    Finished up majority of Learning tree. All graphs and tree learning done. Just need to finish argument inputs. Added jar file for graphi api. need to figure out how to build on linux machine

--- DecisionTree/src/Main.java
+++ DecisionTree/src/Main.java
@@ -1,12 +1,183 @@
+import info.monitorenter.gui.chart.Chart2D;
+import info.monitorenter.gui.chart.IAxis;
+import info.monitorenter.gui.chart.ITrace2D;
+import info.monitorenter.gui.chart.traces.Trace2DSimple;
+
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.WindowAdapter;
+import java.awt.event.WindowEvent;
 import java.io.FileNotFoundException;
 import java.text.ParseException;
+import java.util.List;
+import java.util.ArrayList;
+import java.util.Random;
 
 /**
  * Created by maxhorowitz on 1/24/15.
  */
 public class Main {
-    public static void main(String [] args) throws FileNotFoundException, ParseException {
-        ARFFParser parser = new ARFFParser("/Users/maxhorowitz/Desktop/heart_train.arff.txt");
+    public static void main(String[] args) throws FileNotFoundException, ParseException {
+        ARFFParser trainParser = new ARFFParser("/Users/maxhorowitz/Desktop/heart_train.arff.txt");
+        Split.m = 20;
+        Split root = new Split();
+        root.instancesRemaining = trainParser.instances;
+
+        //root.InfoGain(parser.attributes.get(parser.attributes.size() - 2));
+        root.ChooseBestSplit(trainParser.attributes);
+
+        for (Split child : root.children) {
+            child.Print(root.attributeToSplit, false, 0, 0);
+        }
+        ARFFParser testParser = new ARFFParser("/Users/maxhorowitz/Desktop/heart_test.arff");
+
+
+        int correct = 0;
+        for (String[] instance : testParser.instances) {
+            for (int i = 0; i < instance.length - 1; i++) {
+                System.out.print(instance[i] + " ");
+            }
+            String treeClassification = root.classify(instance);
+            String actualClassification = instance[instance.length - 1];
+            System.out.println("Classified: " + treeClassification + " Actually: " + actualClassification);
+            if (actualClassification.equals(treeClassification)) {
+                correct++;
+            }
+        }
+        System.out.println(correct + " correct out of " + testParser.instances.size());
+
+
+        // Create a chart:
+        Chart2D chart = new Chart2D();
+        chart.getAxisX().setAxisTitle(new IAxis.AxisTitle("Training Size"));
+        chart.getAxisY().setAxisTitle(new IAxis.AxisTitle("Accuracy"));
+        // Create an ITrace:
+        ITrace2D minTrace = new Trace2DSimple();
+        ITrace2D maxTrace = new Trace2DSimple();
+        ITrace2D averageTrace = new Trace2DSimple();
+        minTrace.setName("Minimum Accuracy");
+        minTrace.setColor(Color.blue);
+        averageTrace.setName("Average Accuracy");
+        averageTrace.setColor(Color.green);
+        maxTrace.setName("Maximum Accuracy");
+        maxTrace.setColor(Color.red);
+        // Add the trace to the chart. This has to be done before adding points (deadlock prevention):
+        chart.addTrace(minTrace);
+        chart.addTrace(maxTrace);
+        chart.addTrace(averageTrace);
+
+        // Part 2
+        Split.m = 4;
+        ARFFParser p2TrainParser = new ARFFParser("/Users/maxhorowitz/Desktop/heart_train.arff.txt");
+        ARFFParser p2TestParser = new ARFFParser("/Users/maxhorowitz/Desktop/heart_test.arff");
+
+        List<String[]> positiveInstances = new ArrayList<String[]>();
+        List<String[]> negativeInstances = new ArrayList<String[]>();
+        for (String[] instance : p2TrainParser.instances) {
+            if (instance[instance.length - 1].equals("positive"))
+                positiveInstances.add(instance);
+            else
+                negativeInstances.add(instance);
+        }
+        float ratio = positiveInstances.size() / (float) p2TrainParser.instances.size();
+
+        int[] trainSizes = {25, 50, 100, 200};
+
+
+        for (int i = 0; i < trainSizes.length; i++) {
+            int trainSize = trainSizes[i];
+            float maxAccuracy = Float.NEGATIVE_INFINITY;
+            float minAccuracy = Float.POSITIVE_INFINITY;
+            float accuracySum = 0;
+            for (int k = 0; k < 10; k++) {
+                List<String[]> positiveInstancesCopy = new ArrayList<String[]>(positiveInstances);
+                List<String[]> negativeInstancesCopy = new ArrayList<String[]>(negativeInstances);
+                List<String[]> trainingInstances = new ArrayList<String[]>();
+                if (trainSize != 200) {
+                    for (int j = 0; j < (int)(ratio * trainSize); j++) {
+                        trainingInstances.add(positiveInstancesCopy.remove((int)(Math.random() * positiveInstancesCopy.size())));
+                    }
+                    int negativeSize = trainSize - trainingInstances.size();
+                    for(int j = 0; j < negativeSize; j ++){
+                        trainingInstances.add(negativeInstancesCopy.remove((int)(Math.random() * negativeInstancesCopy.size())));
+                    }
+                }
+                else{
+                    trainingInstances.addAll(positiveInstancesCopy);
+                    trainingInstances.addAll(negativeInstancesCopy);
+                }
+                Split tree = new Split();
+                tree.instancesRemaining = trainingInstances;
+                tree.ChooseBestSplit(p2TestParser.attributes);
+                float accuracy = TestAccuracy(tree, p2TestParser.instances);
+                minAccuracy = Math.min(minAccuracy, accuracy);
+                maxAccuracy = Math.max(maxAccuracy, accuracy);
+                accuracySum += accuracy;
+            }
+            float averageAccuracy = accuracySum / 10;
+            averageTrace.addPoint(trainSize, averageAccuracy);
+            maxTrace.addPoint(trainSize, maxAccuracy);
+            minTrace.addPoint(trainSize, minAccuracy);
+        }
+
+
+        // Make it visible:
+        // Create a frame.
+        JFrame frame = new JFrame("Part 2");
+        // add the chart to the frame:
+        frame.getContentPane().add(chart);
+        frame.setSize(1000, 800);
+        // Enable the termination button [cross on the upper right edge]:
+        frame.addWindowListener(
+                new WindowAdapter() {
+                    public void windowClosing(WindowEvent e) {
+                        System.exit(0);
+                    }
+                }
+        );
+        frame.setVisible(true);
+
+
+        int[] mSizes = {2,5,10,20};
+
+        Chart2D mGraph = new Chart2D();
+        ITrace2D mTrace = new Trace2DSimple();
+        mGraph.addTrace(mTrace);
+        mGraph.getAxisY().setAxisTitle(new IAxis.AxisTitle("Accuracy"));
+        mGraph.getAxisX().setAxisTitle(new IAxis.AxisTitle("m"));
+        for(int i = 0; i < mSizes.length; i ++){
+            Split.m = mSizes[i];
+            Split tree = new Split();
+            tree.instancesRemaining = p2TrainParser.instances;
+            tree.ChooseBestSplit(p2TestParser.attributes);
+            mTrace.addPoint(Split.m, TestAccuracy(tree, p2TestParser.instances));
+        }
+
+        JFrame part3 = new JFrame("Part 3");
+        // add the chart to the frame:
+        part3.getContentPane().add(mGraph);
+        part3.setSize(1000, 800);
+        // Enable the termination button [cross on the upper right edge]:
+        part3.addWindowListener(
+                new WindowAdapter() {
+                    public void windowClosing(WindowEvent e) {
+                        System.exit(0);
+                    }
+                }
+        );
+        part3.setVisible(true);
+
+
 
     }
+
+    private static float TestAccuracy(Split root, List<String[]> testSet) {
+        int correct = 0;
+        for(String[] instance : testSet){
+            if(root.classify(instance).equals(instance[instance.length -1]))
+                correct ++;
+        }
+        return correct / (float) testSet.size();
+    }
+
 }
--- DecisionTree/src/Split.java
+++ DecisionTree/src/Split.java
@@ -1,53 +1,289 @@
+import java.math.BigDecimal;
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.Comparator;
 import java.util.List;
-import ARFFParser.Attribute;
-import ARFFParser.Type;
 /**
  * Created by maxhorowitz on 1/24/15.
  */
 public class Split {
-    public int r_val;
+    public static int m;
     public String s_val;
     public List<Split> children;
     public List<String[]> instancesRemaining;
-    public Attribute attributeToSplit;
+    public ARFFParser.Attribute attributeToSplit;
+    public double threshold;
     public String classVal;
 
-    public void ChooseBestSplit(List<Attribute> attributes){
-        float bestInfoGain = Float.MIN_VALUE;
-        for(Attribute attribute : attributes){
-            if(attribute.type == Type.Nominal){
-                float infoGain = InfoGain(attribute);
+    public void ChooseBestSplit(List<ARFFParser.Attribute> attributes){
+        if(instancesRemaining.size() < m || attributes.size() ==0){
+            int[] classCount = countClasses(instancesRemaining);
+            if(classCount[1] > classCount[0]){
+                classVal = "positive";
+            }
+            else{
+                classVal = "negative";
+            }
+            return;
+        }
+
+
+        double bestInfoGain = Double.NEGATIVE_INFINITY;
+        ARFFParser.Attribute bestAttribute = null;
+        double bestThreshold = 0;
+        for(ARFFParser.Attribute attribute : attributes){
+            if(attribute.name.equals("class")){
+                break;
+            }
+            if(attribute.type == ARFFParser.Type.Nominal){
+                double infoGain = InfoGain(attribute);
+                //System.out.println("Info gain for " + attribute.name + " is " + infoGain);
                 if(infoGain > bestInfoGain){
-                    //bestInfoGain = infoGain;
-                    //infoGain
+                    bestInfoGain = infoGain;
+                    bestAttribute = attribute;
                 }
             }
             else{
                 Collections.sort(instancesRemaining, new InstanceComparator(attribute.index));
+                double infoGain;
                 for(int i = 0; i < instancesRemaining.size() - 1; i ++) {
-                    //infoGain = InfoGain(attribute, threshold);
+                    double instanceA = Double.parseDouble(instancesRemaining.get(i)[attribute.index]);
+                    double instanceB = Double.parseDouble(instancesRemaining.get(i + 1)[attribute.index]);
+                    if(instanceA == instanceB)
+                        continue;
+                    double threshold = (instanceA + instanceB) / 2.0;
+                    infoGain = InfoGain(attribute, threshold);
+                    //System.out.println("Info gain for " + attribute.name + "at threshold "+ threshold + " is " + infoGain);
+
+                    if(infoGain > bestInfoGain){
+                        bestAttribute = attribute;
+                        bestInfoGain = infoGain;
+                        bestThreshold = threshold;
+                    }
                 }
             }
         }
-    }
 
-    public float InfoGain(Attribute attribute){
-        if(attribute.type == Type.Nominal){
+        if(bestInfoGain <= 0){
+            int[] classCount = countClasses(instancesRemaining);
+            if(classCount[1] > classCount[0]){
+                classVal = "positive";
+            }
+            else{
+                classVal = "negative";
+            }
+            return;
+        }
+        if(bestAttribute == null){
+            System.out.println("what the fuck");
+        }
+        this.threshold = bestThreshold;
+        this.attributeToSplit = bestAttribute;
+        List<ARFFParser.Attribute> childAttributes = new ArrayList<ARFFParser.Attribute>(attributes);
+        if(bestAttribute.type != ARFFParser.Type.Real)
+            childAttributes.remove(bestAttribute);
+        children = new ArrayList<Split>();
+        if(attributeToSplit.type == ARFFParser.Type.Nominal){
+            for(String val : attributeToSplit.nominalValues){
+                List<String[]> childInstances = new ArrayList<String[]>();
+                for(int i = 0; i < instancesRemaining.size(); i ++){
+                    if(instancesRemaining.get(i)[attributeToSplit.index].equals(val)){
+                        childInstances.add(instancesRemaining.get(i));
+                    }
+                }
 
+
+                Split child =  new Split();
+                if(childInstances.size() == 0){
+                    child.s_val = val;
+                    int[] classCount = countClasses(instancesRemaining);
+                    if(classCount[1] > classCount[0]){
+                        child.classVal = "positive";
+                    }
+                    else{
+                        child.classVal = "negative";
+                    }
+                }
+                else {
+                    child.s_val = val;
+                    child.instancesRemaining = childInstances;
+                    child.ChooseBestSplit(childAttributes);
+                }
+                children.add(child);
+            }
+        }
+        else if(attributeToSplit.type == ARFFParser.Type.Real){
+            List<String[]> ltEqThreshold = new ArrayList<String[]>();
+            List<String[]> greaterThreshold = new ArrayList<String[]>();
+            for(String[] instance : instancesRemaining){
+                double val = Double.parseDouble(instance[bestAttribute.index]);
+                if(val > threshold){
+                    greaterThreshold.add(instance);
+                }
+                else{
+                    ltEqThreshold.add(instance);
+                }
+            }
+            Split ltEqSplit = new Split();
+            if(ltEqThreshold.size() == 0){
+                int[] classCount = countClasses(instancesRemaining);
+                if(classCount[1] > classCount[0]){
+                    ltEqSplit.classVal = "positive";
+                }
+                else{
+                    ltEqSplit.classVal = "negative";
+                }
+            }
+            else{
+                ltEqSplit.instancesRemaining = ltEqThreshold;
+                ltEqSplit.ChooseBestSplit(childAttributes);
+            }
+            children.add(ltEqSplit);
+            Split greaterSplit = new Split();
+            if(greaterThreshold.size() == 0){
+                int[] classCount = countClasses(instancesRemaining);
+                if(classCount[1] > classCount[0]){
+                    greaterSplit.classVal = "positive";
+                }
+                else{
+                    greaterSplit.classVal = "negative";
+                }
+            }
+            else{
+                greaterSplit.instancesRemaining = greaterThreshold;
+                greaterSplit.ChooseBestSplit(childAttributes);
+            }
+            children.add(greaterSplit);
         }
         else{
-            Collections.sort(instancesRemaining, new InstanceComparator(attribute.index));
-            for(int i = 0; i < instancesRemaining.size() -1; i ++) {
-                return InfoGain()
+            System.out.println("What the fuck");
+        }
+    }
+
+    public int[] countClasses(List<String[]> instances){
+        int[] total = {0,0};
+        for(String[] instance : instances){
+            if(instance[instance.length -1].equals("negative"))
+                total[0] ++;
+            else
+                total[1] ++;
+        }
+        return total;
+    }
+
+    public double InfoGain(ARFFParser.Attribute attribute){
+        double entropy = entropy(instancesRemaining);
+        double conditionalEntropy = 0;
+        for(String val : attribute.nominalValues){
+            List<String[]> instancesWithVal = new ArrayList<String[]>();
+            for(String[] instance : instancesRemaining){
+                if(instance[attribute.index].equals(val)){
+                    instancesWithVal.add(instance);
+                }
+            }
+            double pVal = instancesWithVal.size() / (double) instancesRemaining.size();
+            conditionalEntropy += pVal* entropy(instancesWithVal);
+        }
+        return entropy - conditionalEntropy;
+    }
+
+    public double entropy(List<String[]> instances){
+        int[] classCount = countClasses(instances);
+        if(classCount[1] == 0 || classCount[0] == 0){
+            return 0;
+        }
+        double pNegative = classCount[0] / (double)instances.size();
+        double pPositive = classCount[1] / (double)instances.size();
+        return -(pNegative* log2(pNegative)) - (pPositive * log2(pPositive));
+    }
+
+
+    public static final double LOGOF2 = 0.693147180559945309417232121458176568075500134360255254120680;
+    public double log2(double a){
+        return (double) (Math.log(a) / LOGOF2);
+    }
+
+    public double InfoGain(ARFFParser.Attribute attribute, double threshold){
+        double entropy = entropy(instancesRemaining);
+        double conditionalEntropy = 0;
+        List<String[]> ltEqThreshold = new ArrayList<String[]>();
+        List<String[]> greaterThreshold = new ArrayList<String[]>();
+        for(String[] instance : instancesRemaining){
+            double val = Double.parseDouble(instance[attribute.index]);
+            if(val > threshold){
+                greaterThreshold.add(instance);
+            }
+            else{
+                ltEqThreshold.add(instance);
+            }
+        }
+        double pLtEq = ltEqThreshold.size() / (double) instancesRemaining.size();
+        double pGreater = greaterThreshold.size() / (double) instancesRemaining.size();
+        conditionalEntropy += pLtEq * entropy(ltEqThreshold);
+        conditionalEntropy += pGreater * entropy(greaterThreshold);
+        return entropy - conditionalEntropy;
+    }
+
+    public String classify(String[] instance){
+        if(classVal != null){
+            return classVal;
+        }
+        if(attributeToSplit == null){
+            System.out.println("what the fucl");
+        }
+        if(attributeToSplit.type == ARFFParser.Type.Nominal){
+            for(Split child : children){
+                if(child.s_val == null){
+                    System.out.println("What the fuck");
+                }
+                if(child.s_val.equals(instance[attributeToSplit.index])){
+                    return child.classify(instance);
+                }
+            }
+            return null;
+        }
+        else{
+            double val = Double.parseDouble(instance[attributeToSplit.index]);
+            if(val > threshold){
+                return children.get(1).classify(instance);
+            }
+            else{
+                return  children.get(0).classify(instance);
             }
         }
     }
 
-    public float InfoGain(Attribute attribute, float threshold){
-
+    public void Print(ARFFParser.Attribute attr, boolean greater ,double threshold, int tabs){
+        for(int i = 0; i < tabs; i ++){
+            System.out.print("|\t");
+        }
+        if(attr.type == ARFFParser.Type.Real){
+            System.out.print(attr.name+ " ");
+            if(greater){
+                System.out.print("> " + threshold+" ");
+            }
+            else{
+                System.out.print("<= "+ threshold+" ");
+            }
+        }
+        else{
+            System.out.print(attr.name+" = "+ s_val);
+        }
+        if(classVal != null){
+            System.out.println(": "+ classVal);
+        }
+        else {
+            System.out.println();
+            if(attributeToSplit.type == ARFFParser.Type.Nominal) {
+                for (Split child : children){
+                    child.Print(attributeToSplit, false, 0, tabs + 1);
+                }
+            }
+            else{
+                children.get(0).Print(attributeToSplit,false,this.threshold,tabs+1);
+                children.get(1).Print(attributeToSplit, true,this.threshold,tabs+1);
+            }
+        }
     }
 
     public static class InstanceComparator implements Comparator<String[]>{
@@ -58,13 +294,13 @@ public class Split {
         }
         @Override
         public int compare(String[] o1, String[] o2) {
-            float a = Float.parseFloat(o1[index]);
-            float b = Float.parseFloat(o2[index]);
+            double a = Double.parseDouble(o1[index]);
+            double b = Double.parseDouble(o2[index]);
             if(a > b)
                 return 1;
             else if(a < b)
-                return 0;
-            else return -1;
+                return -1;
+            else return 0;
         }
     }
 }
